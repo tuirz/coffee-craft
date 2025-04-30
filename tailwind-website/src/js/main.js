99e9976aeefc6b0fa0bff8766acdf1075b0cd953
Luigi SOUFAR <basses_bouleau.3n@icloud.com>
2025-04-30 10:38:47 +0200

    bases

--- tailwind-website/src/js/main.js
+++ tailwind-website/src/js/main.js
@@ -1,1 +1,7 @@
-// This file is intentionally left blank.
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('menu-btn');
+    const menu = document.getElementById('menu');
+    btn.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+    });
+  });
